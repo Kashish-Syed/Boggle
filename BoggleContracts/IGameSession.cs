c8d04a60727869081f30ae5136c63300ad8e32ff
Kashish-Syed <ksyed2@huskers.unl.edu>
2024-04-08 19:08:33 -0500

    Added game session classes to Engine and Contracts

---
+++ BoggleContracts/IGameSession.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boggle;
+
+internal interface IGameSession
+{
+    int GetScore()
+    {
+        
+    }
+
+    int GetWinner()
+    {
+        
+    }
+
+    int StartGameSession()
+    {
+        
+    }
+}
---
+++ BoggleEngines/GameSession.cs
@@ -0,0 +1,11 @@
+namespace Boggle;
+
+public class GameSession
+{
+    private double startTime;
+    private double endTime;
+    private double gameDuration;
+    private int score;
+    private string winner;
+    
+}
