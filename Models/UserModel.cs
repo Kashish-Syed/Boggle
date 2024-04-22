1ec893cc45dc20f0d7f4e9ec2bbe259b093c6c50
Marie Zhussupova <102762019+phychee@users.noreply.github.com>
2024-04-22 18:54:26 -0500

    Added json properties to User model

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,16 +21,19 @@ namespace Models
         /// <summary>
         /// User Guid
         /// </summary>
+        [JsonProperty("id")]
         public Guid? Id { get; set; }
 
         /// <summary>
         /// The user's username
         /// </summary>
+        [JsonProperty("username")]
         public string? UserName { get; set; }
 
         /// <summary>
         /// The list of words the user entered
         /// </summary>
+        [JsonProperty("words")]
         public List<string>? Words { get; set; }
     }
 }
