b890362474ce0ff51a14d1b42ff05c9b2f84dc89
Dawson McGahan <dmcgahan04@gmail.com>
2024-05-06 23:57:19 -0500

    fixed the problems lol

--- src/BoggleAccessors/DatabaseGameInfo.cs
+++ src/BoggleAccessors/DatabaseGameInfo.cs
@@ -3,104 +3,99 @@ using System.Data;
 using System.Data.SqlClient;
 using BoggleContracts;
 using BoggleEngines;
+using System.Linq;
 
 namespace BoggleAccessors
 {
     public class DatabaseGameInfo : IDatabaseGameInfo
     {
         private readonly string _connectionString = @"Server=localhost\SQLEXPRESS;Database=boggle;Trusted_Connection=True;";
-        private SqlConnection _connection;
-
-        public DatabaseGameInfo()
-        {
-            _connection = new SqlConnection(_connectionString);
-            _connection.Open();
-        }
 
         public string CreateGame()
         {
-            IGameDice dice = new GameDice();
-            var gameCode = GenerateGameCode();
-            var board = dice.ShuffleAllDice();
-
-            var boardString = new string(board); 
-
-            using (SqlCommand command = new SqlCommand("INSERT INTO Game (GameCode, Board) VALUES (@GameCode, @Board)", _connection))
+            using (var connection = new SqlConnection(_connectionString))
             {
-                command.Parameters.AddWithValue("@GameCode", gameCode);
-                command.Parameters.AddWithValue("@Board", boardString);
-                command.ExecuteNonQuery();
-            }
+                connection.Open();
+                var dice = new GameDice();
+                var gameCode = GenerateGameCode();
+                var board = dice.ShuffleAllDice();
+                var boardString = new string(board);
 
-            return gameCode;
+                using (var command = new SqlCommand("INSERT INTO Game (GameCode, Board) VALUES (@GameCode, @Board)", connection))
+                {
+                    command.Parameters.AddWithValue("@GameCode", gameCode);
+                    command.Parameters.AddWithValue("@Board", boardString);
+                    command.ExecuteNonQuery();
+                }
+
+                return gameCode;
+            }
         }
 
         public int DeleteGame(string gameCode)
         {
-            using (SqlCommand command = new SqlCommand("DELETE FROM GameWord WHERE GameCode = @GameCode", _connection))
+            using (var connection = new SqlConnection(_connectionString))
             {
-                command.Parameters.AddWithValue("@GameCode", gameCode);
-                command.ExecuteNonQuery(); 
-            }
-
-            using (SqlCommand command = new SqlCommand("DELETE FROM GamePlayer WHERE GameCode = @GameCode", _connection))
-            {
-                command.Parameters.AddWithValue("@GameCode", gameCode);
-                command.ExecuteNonQuery();
-            }
-
-            using (SqlCommand command = new SqlCommand("DELETE FROM Game WHERE GameCode = @GameCode", _connection))
-            {
-                command.Parameters.AddWithValue("@GameCode", gameCode);
-                return command.ExecuteNonQuery();
+                connection.Open();
+                using (var command = new SqlCommand("DELETE FROM GameWord WHERE GameCode = @GameCode; DELETE FROM GamePlayer WHERE GameCode = @GameCode; DELETE FROM Game WHERE GameCode = @GameCode;", connection))
+                {
+                    command.Parameters.AddWithValue("@GameCode", gameCode);
+                    return command.ExecuteNonQuery();  // This executes all deletes in one command.
+                }
             }
         }
 
-
         public char[] GetBoard(string gameCode)
         {
-            using (SqlCommand command = new SqlCommand("SELECT Board FROM Game WHERE GameCode = @GameCode", _connection))
+            using (var connection = new SqlConnection(_connectionString))
             {
-                command.Parameters.AddWithValue("@GameCode", gameCode);
-                var boardString = command.ExecuteScalar() as string;
-                return boardString.ToCharArray();
+                connection.Open();
+                using (var command = new SqlCommand("SELECT Board FROM Game WHERE GameCode = @GameCode", connection))
+                {
+                    command.Parameters.AddWithValue("@GameCode", gameCode);
+                    var boardString = command.ExecuteScalar() as string;
+                    return boardString?.ToCharArray();
+                }
             }
         }
 
         public void AddPlayer(string gameCode, string username)
         {
-            using (SqlCommand command = new SqlCommand(
-                @"DECLARE @PlayerID INT
-                SELECT @PlayerID = PlayerID FROM Player WHERE Username = @Username
-                IF @PlayerID IS NOT NULL
-                BEGIN
-                    INSERT INTO GamePlayer (GameCode, PlayerID) VALUES (@GameCode, @PlayerID)
-                END", _connection))
+            using (var connection = new SqlConnection(_connectionString))
             {
-                command.Parameters.AddWithValue("@GameCode", gameCode);
-                command.Parameters.AddWithValue("@Username", username);
-                _connection.Open();
-                command.ExecuteNonQuery();
-                _connection.Close();
+                connection.Open();
+                using (var command = new SqlCommand(
+                    @"DECLARE @PlayerID INT;
+                    SELECT @PlayerID = PlayerID FROM Player WHERE Username = @Username;
+                    IF @PlayerID IS NOT NULL
+                    BEGIN
+                        INSERT INTO GamePlayer (GameCode, PlayerID) VALUES (@GameCode, @PlayerID);
+                    END", connection))
+                {
+                    command.Parameters.AddWithValue("@GameCode", gameCode);
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.ExecuteNonQuery();
+                }
             }
         }
 
-
-        public string GetWinner(string gameCode) 
+        public string GetWinner(string gameCode)
         {
-            using (SqlCommand command = new SqlCommand(
-                @"SELECT TOP 1 p.Username
-                FROM GamePlayer gp
-                JOIN Player p ON gp.PlayerID = p.PlayerID
-                WHERE gp.GameCode = @GameCode
-                ORDER BY gp.TotalScore DESC", _connection))
+            using (var connection = new SqlConnection(_connectionString))
             {
-                command.Parameters.AddWithValue("@GameCode", gameCode);
-                var winner = command.ExecuteScalar() as string;
-                return winner;
+                connection.Open();
+                using (var command = new SqlCommand(
+                    @"SELECT TOP 1 p.Username
+                    FROM GamePlayer gp
+                    JOIN Player p ON gp.PlayerID = p.PlayerID
+                    WHERE gp.GameCode = @GameCode
+                    ORDER BY gp.TotalScore DESC", connection))
+                {
+                    command.Parameters.AddWithValue("@GameCode", gameCode);
+                    return command.ExecuteScalar() as string;
+                }
             }
         }
-
 
         private string GenerateGameCode()
         {
@@ -108,14 +103,6 @@ namespace BoggleAccessors
             var random = new Random();
             return new string(Enumerable.Repeat(chars, 6)
                                         .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
-
-        ~DatabaseGameInfo()
-        {
-            if (_connection != null && _connection.State == ConnectionState.Open)
-            {
-                _connection.Close();
-            }
         }
     }
 }
--- src/BoggleAccessors/DatabaseWordInfo.cs
+++ src/BoggleAccessors/DatabaseWordInfo.cs
@@ -9,116 +9,83 @@ namespace BoggleAccessors
     public class DatabaseWordInfo : IDatabaseWordInfo
     {
         private readonly string _connectionString = @"Server=localhost\SQLEXPRESS;Database=boggle;Trusted_Connection=True;";
-        private SqlConnection _connection;
-
-        public DatabaseWordInfo()
-        {
-            _connection = new SqlConnection(_connectionString);
-            _connection.Open();
-        }
 
         public void AddWordsToDatabase(string filepath)
-{
-    try
-    {
-        using (StreamReader sr = new StreamReader(filepath))
         {
-            string line;
-            while ((line = sr.ReadLine()) != null)
+            try
             {
-                line = line.Trim();
-                if (!string.IsNullOrEmpty(line) && line.All(char.IsLetter))
+                using (StreamReader sr = new StreamReader(filepath))
                 {
-                    int points = CalculatePoints(line);
-                    if (points > 0)
+                    using (var connection = new SqlConnection(_connectionString))
                     {
-                        try
+                        connection.Open();
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
                         {
-                            using (SqlCommand command = new SqlCommand("INSERT INTO Word (Word, Points) VALUES (@Word, @Points)", _connection))
+                            line = line.Trim();
+                            if (!string.IsNullOrEmpty(line) && line.All(char.IsLetter))
                             {
-                                command.Parameters.AddWithValue("@Word", line.ToLower());
-                                command.Parameters.AddWithValue("@Points", points);
-                                command.ExecuteNonQuery();
+                                int points = CalculatePoints(line);
+                                if (points > 0)
+                                {
+                                    try
+                                    {
+                                        using (SqlCommand command = new SqlCommand("INSERT INTO Word (Word, Points) VALUES (@Word, @Points)", connection))
+                                        {
+                                            command.Parameters.AddWithValue("@Word", line.ToLower());
+                                            command.Parameters.AddWithValue("@Points", points);
+                                            command.ExecuteNonQuery();
+                                        }
+                                    }
+                                    catch (SqlException ex)
+                                    {
+                                        if (ex.Number == 2627) // Handle duplicate primary key error
+                                        {
+                                            Console.WriteLine("Duplicate word skipped: " + line);
+                                        }
+                                        else
+                                        {
+                                            throw;
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Invalid word: " + line);
+                                }
                             }
                         }
-                        catch (SqlException ex)
-                        {
-                            if (ex.Number == 2627) // 2627 is the SQL error code for a duplicate primary key value, need so the program doesn't crash on repeat words
-                            {
-                                Console.WriteLine("Duplicate word skipped: " + line);
-                            }
-                            else
-                            {
-                                throw;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid word: " + line);
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Failed to add words to the database: {ex.Message}", ex);
+            }
         }
-    }
-    catch (Exception ex)
-    {
-        throw new InvalidOperationException($"Failed to add words to the database: {ex.Message}", ex);
-    }
-}
-
 
         private int CalculatePoints(string word)
         {
             int wordLength = word.Length;
-            if (wordLength <= 2)
-            {
-                return 0; 
-            }
-            if (wordLength == 3 || wordLength == 4)
-            {
-                return 1;
-            }
-            else if (wordLength == 5)
-            {
-                return 2;
-            }
-            else if (wordLength == 6)
-            {
-                return 3;
-            }
-            else if (wordLength == 7)
-            {
-                return 5;
-            }
-            else
-            {
-                return 11; 
-            }
+            if (wordLength <= 2) return 0;
+            if (wordLength == 3 || wordLength == 4) return 1;
+            if (wordLength == 5) return 2;
+            if (wordLength == 6) return 3;
+            if (wordLength == 7) return 5;
+            return 11;  // 8 or more letters
         }
 
         public int GetWordID(string word)
         {
-            try
+            using (var connection = new SqlConnection(_connectionString))
             {
-                using (SqlCommand command = new SqlCommand("SELECT WordID FROM Word WHERE Word = @Word", _connection))
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT WordID FROM Word WHERE Word = @Word", connection))
                 {
                     command.Parameters.AddWithValue("@Word", word);
                     object result = command.ExecuteScalar();
                     return result != null ? Convert.ToInt32(result) : -1;
                 }
-            }
-            catch (SqlException ex)
-            {
-                throw new InvalidOperationException($"Failed to retrieve Word ID: {ex.Message}", ex);
-            }
-        }
-
-        ~DatabaseWordInfo()
-        {
-            if (_connection != null && _connection.State == ConnectionState.Open)
-            {
-                _connection.Close();
             }
         }
     }
