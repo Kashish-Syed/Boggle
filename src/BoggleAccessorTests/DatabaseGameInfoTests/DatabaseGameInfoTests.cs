078def80700d32a57b968aab04f997e9ea23caa9
Dawson McGahan <dmcgahan04@gmail.com>
2024-05-09 16:53:38 -0500

    fixed connection string handling

--- src/BoggleAccessorTests/DatabaseGameInfoTests/DatabaseGameInfoTests.cs
+++ src/BoggleAccessorTests/DatabaseGameInfoTests/DatabaseGameInfoTests.cs
@@ -9,6 +9,7 @@ namespace BoggleAccessorTests.DatabaseGameInfoTests
     public class DatabaseGameInfoTests
     {
         private DatabaseGameInfo _dbGameInfo;
+        
 
         [SetUp]
         public void Setup()
@@ -19,28 +20,28 @@ namespace BoggleAccessorTests.DatabaseGameInfoTests
         [Test]
         public void CreateGameCreatesValidGame()
         {
-            string gameCode = _dbGameInfo.CreateGame();
+            string gameCode = _dbGameInfo.CreateGameAsync();
             Assert.That(gameCode, Is.Not.EqualTo(null), "Game code should not be null");
             Assert.That(gameCode.Length, Is.EqualTo(6), "Game code should be 6 characters long");
 
-            char[] board = _dbGameInfo.GetBoard(gameCode);
+            char[] board = _dbGameInfo.GetBoardAsync(gameCode);
             Assert.That(board, Is.Not.EqualTo(null), "Board should not be null");
             Assert.That(board.Length, Is.EqualTo(16), "Board should contain 16 characters");
 
-            int deleteResult = _dbGameInfo.DeleteGame(gameCode);
+            int deleteResult = _dbGameInfo.DeleteGameAsync(gameCode);
             Assert.That(deleteResult, Is.EqualTo(1), "Game was not deleted");
         }
 
         [Test]
         public void GetBoardReturnsCorrectBoard()
         {
-            string gameCode = _dbGameInfo.CreateGame();
-            char[] expectedBoard = _dbGameInfo.GetBoard(gameCode);
+            string gameCode = _dbGameInfo.CreateGameAsync();
+            char[] expectedBoard = _dbGameInfo.GetBoardAsync(gameCode);
 
-            char[] retrievedBoard = _dbGameInfo.GetBoard(gameCode);
+            char[] retrievedBoard = _dbGameInfo.GetBoardAsync(gameCode);
             Assert.That(retrievedBoard, Is.EqualTo(expectedBoard), "Retrieved board did not match stored board");
 
-            _dbGameInfo.DeleteGame(gameCode);
+            _dbGameInfo.DeleteGameAsync(gameCode);
         }
     }
 }
