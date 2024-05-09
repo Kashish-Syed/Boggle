1a46d8a013bb4aa11d425ff2826f8fa81b62b258
Marie Zhussupova <102762019+phychee@users.noreply.github.com>
2024-05-09 12:47:31 -0500

    added DI to dbplayerinfo

--- src/BoggleAccessors/DatabasePlayerInfo.cs
+++ src/BoggleAccessors/DatabasePlayerInfo.cs
@@ -7,36 +7,35 @@ namespace BoggleAccessors
 {
     public class DatabasePlayerInfo : IDatabasePlayerInfo
     {
-        private readonly string _connectionString = @"Server=localhost\SQLEXPRESS;Database=boggle;Trusted_Connection=True;";
+        private readonly SqlConnection _connection;
+
+        public DatabasePlayerInfo(SqlConnection connection)
+        {
+            _connection = connection;
+        }
 
         public void AddPlayer(string username, string password)
         {
-            using (var connection = new SqlConnection(_connectionString))
+            connection.Open();
+            using (var command = new SqlCommand("INSERT INTO Player (Username, Password) VALUES (@Username, @Password)", connection))
             {
-                connection.Open();
-                using (var command = new SqlCommand("INSERT INTO Player (Username, Password) VALUES (@Username, @Password)", connection))
-                {
-                    command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@Password", password);
-                    command.ExecuteNonQuery();
-                }
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+                command.ExecuteNonQuery();
             }
         }
 
         public string GetUsername(int userId)
         {
-            using (var connection = new SqlConnection(_connectionString))
+            connection.Open();
+            using (var command = new SqlCommand("SELECT Username FROM Player WHERE PlayerID = @PlayerID", connection))
             {
-                connection.Open();
-                using (var command = new SqlCommand("SELECT Username FROM Player WHERE PlayerID = @PlayerID", connection))
-                {
-                    command.Parameters.AddWithValue("@PlayerID", userId);
-                    object result = command.ExecuteScalar();
-                    if (result != null)
-                        return result.ToString();
-                    else
-                        throw new InvalidOperationException("No user found with the specified ID.");
-                }
+                command.Parameters.AddWithValue("@PlayerID", userId);
+                object result = command.ExecuteScalar();
+                if (result != null)
+                    return result.ToString();
+                else
+                    throw new InvalidOperationException("No user found with the specified ID.");
             }
         }
 
@@ -46,51 +45,42 @@ namespace BoggleAccessors
             if (userId == -1)
                 throw new ArgumentException("Authentication failed. Player not found.");
                 
-            using (var connection = new SqlConnection(_connectionString))
+            connection.Open();
+            using (var command = new SqlCommand("DELETE FROM Player WHERE Username = @Username AND Password = @Password", connection))
             {
-                connection.Open();
-                using (var command = new SqlCommand("DELETE FROM Player WHERE Username = @Username AND Password = @Password", connection))
-                {
-                    command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@Password", password);
-                    command.ExecuteNonQuery();
-                }
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+                command.ExecuteNonQuery();
             }
         }
 
         public int Authenticate(string username, string password)
         {
-            using (var connection = new SqlConnection(_connectionString))
+            connection.Open();
+            using (var command = new SqlCommand("SELECT PlayerID FROM Player WHERE Username = @Username AND Password = @Password", connection))
             {
-                connection.Open();
-                using (var command = new SqlCommand("SELECT PlayerID FROM Player WHERE Username = @Username AND Password = @Password", connection))
-                {
-                    command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@Password", password);
-                    object result = command.ExecuteScalar();
-                    return result != null ? Convert.ToInt32(result) : -1;
-                }
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+                object result = command.ExecuteScalar();
+                return result != null ? Convert.ToInt32(result) : -1;
             }
         }
 
         public DataTable GetGames(string username)
         {
             DataTable gamesTable = new DataTable();
-            using (var connection = new SqlConnection(_connectionString))
+            connection.Open();
+            using (var command = new SqlCommand(
+                "SELECT g.GameCode, gp.TotalScore " +
+                "FROM GamePlayer gp " +
+                "JOIN Player p ON gp.PlayerID = p.PlayerID " +
+                "JOIN Game g ON gp.GameCode = g.GameCode " +
+                "WHERE p.Username = @Username", connection))
             {
-                connection.Open();
-                using (var command = new SqlCommand(
-                    "SELECT g.GameCode, gp.TotalScore " +
-                    "FROM GamePlayer gp " +
-                    "JOIN Player p ON gp.PlayerID = p.PlayerID " +
-                    "JOIN Game g ON gp.GameCode = g.GameCode " +
-                    "WHERE p.Username = @Username", connection))
+                command.Parameters.AddWithValue("@Username", username);
+                using (var adapter = new SqlDataAdapter(command))
                 {
-                    command.Parameters.AddWithValue("@Username", username);
-                    using (var adapter = new SqlDataAdapter(command))
-                    {
-                        adapter.Fill(gamesTable);
-                    }
+                    adapter.Fill(gamesTable);
                 }
             }
             return gamesTable;
@@ -99,21 +89,18 @@ namespace BoggleAccessors
         public DataTable GetWordsPlayed(string gameCode, string username)
         {
             DataTable wordsTable = new DataTable();
-            using (var connection = new SqlConnection(_connectionString))
+            connection.Open();
+            using (var command = new SqlCommand(
+                "SELECT w.Word, w.Points FROM GameWord gw " +
+                "JOIN Player p ON gw.PlayerID = p.PlayerID " +
+                "JOIN Word w ON gw.WordID = w.WordID " +
+                "WHERE gw.GameCode = @GameCode AND p.Username = @Username", connection))
             {
-                connection.Open();
-                using (var command = new SqlCommand(
-                    "SELECT w.Word, w.Points FROM GameWord gw " +
-                    "JOIN Player p ON gw.PlayerID = p.PlayerID " +
-                    "JOIN Word w ON gw.WordID = w.WordID " +
-                    "WHERE gw.GameCode = @GameCode AND p.Username = @Username", connection))
+                command.Parameters.AddWithValue("@GameCode", gameCode);
+                command.Parameters.AddWithValue("@Username", username);
+                using (var adapter = new SqlDataAdapter(command))
                 {
-                    command.Parameters.AddWithValue("@GameCode", gameCode);
-                    command.Parameters.AddWithValue("@Username", username);
-                    using (var adapter = new SqlDataAdapter(command))
-                    {
-                        adapter.Fill(wordsTable);
-                    }
+                    adapter.Fill(wordsTable);
                 }
             }
             return wordsTable;
