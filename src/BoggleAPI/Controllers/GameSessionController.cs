dcac68ab1d056e75e86da467957fb9de22a0ce70
Marie Zhussupova <102762019+phychee@users.noreply.github.com>
2024-05-12 03:47:23 -0500

    controller refactoring

---
+++ src/BoggleAPI/Controllers/GameSessionController.cs
@@ -0,0 +1,51 @@
+﻿// ----------------------------------------------------------------------------------------------------
+// Project: Boggle
+// Class: GameSessionController.cs
+// GitHub: https://github.com/Kashish-Syed/Boggle
+//
+// Description: Controller class for providing API endpoints for managing the game session server.
+// ----------------------------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using BoggleContracts;
+using BoggleEngines;
+using System;
+using System.Data;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace BoggleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameSessionController : ControllerBase
+    {
+        [HttpGet("session/startGame")]
+        public IActionResult StartMultiplayerGame()
+        {
+            try
+            {
+                _boggleServer.StartGame();
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+
+        [HttpGet("session/endGame")]
+        public IActionResult EndMUltiplayerGame()
+        {
+            try
+            {
+                _boggleServer.EndGame(null);
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+    }
+}
